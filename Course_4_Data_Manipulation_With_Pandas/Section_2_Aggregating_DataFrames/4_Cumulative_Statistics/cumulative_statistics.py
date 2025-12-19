dbeca970458b7b7e6fb6027ac29ed537e5969b4a
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-12-19 16:34:22 -0500

    beginning to use counting

--- Course_4_Data_Manipulation_With_Pandas/Section_2_Aggregating_DataFrames/4_Cumulative_Statistics/cumulative_statistics.py
+++ Course_4_Data_Manipulation_With_Pandas/Section_2_Aggregating_DataFrames/4_Cumulative_Statistics/cumulative_statistics.py
@@ -14,4 +14,20 @@ sales_dataFrame = pd.read_csv('../sales_subset.csv', index_col=0)
 print(sales_dataFrame.head())
 
 # Print the whole data in the tabular source
-print(sales_dataFrame)
+#print(sales_dataFrame)
+
+sales_1_1 = sales_dataFrame[(sales_dataFrame['store'] == 1) & (sales_dataFrame['department'] == 1)]
+print(sales_1_1.head())
+
+
+# Sort sales_1_1 by date
+sales_1_1 = sales_1_1.sort_values(by='date', ascending=True)
+
+# Get the cumulative sum of weekly_sales, add as cum_weekly_sales col
+sales_1_1['cum_weekly_sales'] = sales_1_1['weekly_sales'].cumsum()
+
+# Get the cumulative max of weekly_sales, add as cum_max_sales col
+sales_1_1['cum_max_sales'] = sales_1_1['weekly_sales'].cummax()
+
+# See the columns you calculated
+print(sales_1_1[["date", "weekly_sales", "cum_weekly_sales", "cum_max_sales"]])
